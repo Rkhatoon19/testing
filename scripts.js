493b97c6104c562bfac2499cecfeaae18f569e41
Rubi <rubina.khatoon.7587@gmail.com>
2022-03-18 23:22:44 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+function clickChanged()
+{
+    document.getElementById('change').innerHTML="On Processing";
+} 
+
+
+function addition(a,b)
+{
+    return a+b;
+}
+document.getElementById('add').innerHTML=addition(70,20);
